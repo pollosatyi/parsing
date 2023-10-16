a4fb8d2539d48bbf196d3f42ac485092215c01a3
pollosatyi <pollosatyi@mail.ru>
2023-10-16 21:11:46 +0300

    Рабочий без красивого оформления класса

---
+++ parsing/ParseClass.cs
@@ -0,0 +1,44 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parsing
+{
+    public class ParseClass
+    {
+        public string _nameSite { get; set; }
+
+        public string _address { get; set; }
+
+        public string _cellSelector { get; set; }
+
+        public ParseClass(string name, string address, string cellSelector)
+        {
+            _nameSite = name;
+            _address = address;
+            _cellSelector = cellSelector;
+        }
+
+        public async Task FunctionParsing(ParseClass site)
+        {
+            Console.WriteLine(site._nameSite);
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+
+            var document = await context.OpenAsync(site._address);
+            var cells = document.QuerySelectorAll(site._cellSelector);
+            var titles = cells.Select(m => m.TextContent);
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title);
+            }
+
+        }
+    }
+
+
+}
--- parsing/Program.cs
+++ parsing/Program.cs
@@ -6,19 +6,27 @@ namespace parsing
     {
         static async Task Main(string[] args)
         {
-            var config = Configuration.Default.WithDefaultLoader();
-            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
-            var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(address);
-            var cellSelector = "tr.vevent td:nth-child(3)";
-            var cells = document.QuerySelectorAll(cellSelector);
-            var titles = cells.Select(m => m.TextContent);
 
-
-            foreach( var title in titles )
+            while (true)
             {
-                Console.WriteLine(title);
+                Console.WriteLine("Выберите сайт для парсинга");
+                int NumberOfSiteSelection = int.Parse(Console.ReadLine());
+                switch (NumberOfSiteSelection)
+                {
+                    case 0:
+                        ParseClass wikipedia = new ParseClass("Википедия",
+                            "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes",
+                            "tr.vevent td:nth-child(3)");
+                        wikipedia.FunctionParsing(wikipedia);
+                        break;
+                    case 1:
+                        break;
+
+                }
+
             }
         }
+
+
     }
 }
