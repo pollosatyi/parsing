c80ec701985f41162a8a52bfc25c4025b74fb3e4
pollosatyi <pollosatyi@mail.ru>
2023-10-22 18:05:54 +0300

    не работает гитхаб

--- parsing/Program.cs
+++ parsing/Program.cs
@@ -9,6 +9,7 @@ namespace parsing
 
             while (true)
             {
+
                 Console.WriteLine("Введите название сайта для парсинга");
                 ParseClass parseClass = new ParseClass();
                 parseClass._nameSite = Console.ReadLine();
